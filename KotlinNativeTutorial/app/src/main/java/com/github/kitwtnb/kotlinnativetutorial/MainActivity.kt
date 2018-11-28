ff24dd98547834051e3e282aef494e3be07a4d9f
Keita Watanabe <Nal3eCh@gmail.com>
2018-11-28 18:30:40 +0900

    add android module

--- KotlinNativeTutorial/app/src/main/java/com/github/kitwtnb/kotlinnativetutorial/MainActivity.kt
+++ KotlinNativeTutorial/app/src/main/java/com/github/kitwtnb/kotlinnativetutorial/MainActivity.kt
@@ -2,11 +2,14 @@ package com.github.kitwtnb.kotlinnativetutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.github.kitwtnb.kotlinnativetutorial.common.createApplicationScreenMessage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
     }
 }
