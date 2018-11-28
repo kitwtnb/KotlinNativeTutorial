b05dac27536407ca83c76fe6c787286b72cd6841
Keita Watanabe <Nal3eCh@gmail.com>
2018-11-28 18:46:15 +0900

    add iOS module

--- KotlinNativeTutorial/iOS/KotlinNativeTutorial/ViewController.swift
+++ KotlinNativeTutorial/iOS/KotlinNativeTutorial/ViewController.swift
@@ -4,14 +4,19 @@
 //
 
 import UIKit
+import ShareCode
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.font = label.font.withSize(25)
+        label.text = CommonKt.createApplicationScreenMessage()
+        self.view.addSubview(label)
     }
-
-
 }
 
